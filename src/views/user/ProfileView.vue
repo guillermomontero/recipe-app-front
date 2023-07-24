3890a0628bc079e0cb1ff3713c0558fe0eb1dd18
Guillermo Montero <gmontero@imaster.golf>
2023-07-24 16:48:01 +0200

    WIP profile

--- src/views/user/ProfileView.vue
+++ src/views/user/ProfileView.vue
@@ -1,3 +1,24 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { useAuthStore } from '../../store/auth';
+import { apiGetUser } from "../../config/api/user";
+
+const store = useAuthStore();
+
+const getUserData = async () => {
+  try {
+    const response = await apiGetUser(store.user._id);
+    console.log(response);
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+onMounted(() => {
+  getUserData();
+})
+</script>
+
 <template>
   <h1>Profile view</h1>
 </template>
